4390b9ded340eccc297d3e3f4268a2d8ea4dfad9
Badr Chanaa <bchanaa@c4r6p4.1337.ma>
2024-11-19 18:06:31 +0100

    M02 ex01 naiveapproach

--- M02/ex01/Fixed.hpp
+++ M02/ex01/Fixed.hpp
@@ -1,12 +1,20 @@
 #ifndef __FIXED_HPP__
 # define __FIXED_HPP__
 
+# include <string>
+
 class Fixed
 {
 	public:
 		Fixed();
+		Fixed(const int i);
+		Fixed(const float f);
 		Fixed(const Fixed &fx);
 		Fixed&	operator=(const Fixed &fx);
+
+		std::string	toString(void) const;
+		int		toInt(void) const;
+		float	toFloat(void) const;
 		int 	getRawBits( void ) const;
 		void	setRawBits( int const raw );
 		~Fixed();
@@ -14,5 +22,7 @@ class Fixed
 		int	_value;
 		static const int _pointPos;
 };
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fx);
 
 #endif
