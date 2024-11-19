fd0f3c09ad58e7df2f7dbe039ed764234a129517
Badr Chanaa <bchanaa@c4r6p4.1337.ma>
2024-11-19 20:45:03 +0100

    M02 ex01

--- M02/ex01/Fixed.cpp
+++ M02/ex01/Fixed.cpp
@@ -9,7 +9,6 @@ Fixed::Fixed(void): _value(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-
 Fixed::Fixed(const Fixed &fx)
 {
 	std::cout << "Copy constructor called" << std::endl;
@@ -22,81 +21,28 @@ Fixed::Fixed(const int i)
 	this->_value = i << 8;
 }
 
-unsigned char	floatToFixed(float frac)
-{
-	unsigned char uc;
-	float	n;
-	n = 0;
-	int i = 1;
-	uc = 0;
-	while (i <= 8)
-	{
-		float t = (float)1 / (2 * i);
-		if ((n + t) > frac)
-		{
-			i++;
-			continue;
-		}
-		else
-		{
-			uc |= (128 >> (i - 1));
-			n += t;
-		}
-		i++;
-	}
-	return uc;
-}
-
-float	fixedToFloat(int val)
-{
-	float f;
-	f = 0;
-	unsigned char fixed;
-	fixed = val & 255;
-	int i = 1;
-	while (i <= 8)
-	{
-		if (fixed & (128 >> (i - 1)))
-			f += (float)1 / (2 * i);
-		i++;
-	}
-	return f;
-}
-
 Fixed::Fixed(const float f)
 {
 	std::cout << "Float constructor called" << std::endl;
-	float rounded = roundf(f);
-	this->_value = ((int) rounded) << 8;
-	float ff = f - rounded;
-	// int i = 0;
-	// while (i < Fixed::_pointPos && ff > roundf(ff))
-	// {
-	// 	std::cout << ff << " > " << roundf(ff) << std::endl;
-	// 	ff *= 10;
-	// 	i++;
-	// }
-	// std::cout << "float " << ff << std::endl;
-	this->_value |= floatToFixed(ff);
+	this->_value = (int) roundf((f * 256));
 }
 
 int	Fixed::toInt(void) const
 {
-	// return this->_value;
 	return this->_value >> 8;
 }
 
 float	Fixed::toFloat(void) const
 {
 	float f;
-	f = this->_value >> 8;
-	f += fixedToFloat(this->_value);
+	int	e = 1 << this->_pointPos;
+	f = (float) this->_value / e;
 	return (f);
 }
 
 int	Fixed::getRawBits( void ) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	// std::cout << "getRawBits member function called" << std::endl;
 	return this->_value;
 }
  
